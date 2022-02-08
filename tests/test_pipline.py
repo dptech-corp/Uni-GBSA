8f379e2f9c951cf9ba9d8692b09c811bc1997b4d
MaohuaYang <maohuay@hotmail.com>
2022-02-08 18:58:58 +0800

    fix: reconstruct
    Former-commit-id: d224c81638160d743289902f31315cbbeca39731 [formerly b7ea1e170e325c2b31afb12f5daf239c9cf9f5d0] [formerly 86b47a4b0b82f90e8dfed77e4f09382b58558c3e [formerly 71641655ca849e079c7c01b950051cd1d99d306a]]
    Former-commit-id: 86b47a4b0b82f90e8dfed77e4f09382b58558c3e
    Former-commit-id: a10457431c1eb8c558790a774b17080ca665ae5b

---
+++ tests/test_pipline.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+
+class TestPipline(unittest.TestCase):
+    def pipline_single(self, pdbfile, rID, lID):
+        pdbname = os.path.split(pdbfile)[-1]
+        #cmd = 'hmtmmpbsa -i %s -o /tmp/%s > hmtmmpbsa.log 2>&1 << EOF \n %d,%d \n EOF'%(pdbfile, pdbname, rID, lID)
+        cmd = 'hmtmmpbsa -i %s -o /tmp/%s  << EOF \n %d,%d \n EOF'%(pdbfile, pdbname, rID, lID)
+        print(cmd)
+        os.system(cmd)
+        EF = os.path.exists('/tmp/%s/FINAL_RESULTS_MMPBSA.dat'%pdbname)
+        self.assertTrue(EF)
+        os.system('rm -rf /tmp/%s/'%pdbname)
+    
+    def pipline_traj(self, pdbfile, trajfile, rID, lID):
+        pdbname = os.path.split(pdbfile)[-1]
+        #cmd = 'hmtmmpbsa -i %s -o /tmp/%s > hmtmmpbsa.log 2>&1 << EOF \n %d,%d \n EOF'%(pdbfile, pdbname, rID, lID)
+        cmd = 'hmtmmpbsa -i %s -t %s -o /tmp/%s  << EOF \n %d,%d \n EOF'%(pdbfile, trajfile, pdbname, rID, lID)
+        print(cmd)
+        os.system(cmd)
+        EF = os.path.exists('/tmp/%s/FINAL_RESULTS_MMPBSA.dat'%pdbname)
+        self.assertTrue(EF)
+        os.system('rm -rf /tmp/%s/'%pdbname)
+
+    def test_2fvy(self):
+        self.pipline_single('../example/2fvy.pdb', 1, 2)
+    
+    def test_traj_ST(self):
+        self.pipline_traj('../example/ST/com.tpr', '../example/ST/com_traj.xtc', 1, 2)
+
+if __name__ == "__main__":
+    unittest.main()
