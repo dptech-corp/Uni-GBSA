581c7200157d7be171c770c7829113522c274f15
MaohuaYang <maohuay@hotmail.com>
2022-03-29 10:23:35 +0800

    refactor: remove old code

--- tests/test_pipline.py
+++ tests/test_pipline.py
@@ -14,34 +14,6 @@ class TestPipline(unittest.TestCase):
         os.mkdir(pdbname)
         return pdbfile, ligandfile, pdbname
 
-#################### version 0.0.1 ########################
-
-    def pipline_single(self, pdbfile, rID, lID):
-        pdbname = os.path.split(pdbfile)[-1]
-        #cmd = 'hmtpbsa -i %s -o /tmp/%s > hmtpbsa.log 2>&1 << EOF \n %d,%d \n EOF'%(pdbfile, pdbname, rID, lID)
-        cmd = 'hmtpbsa -i %s -o /tmp/%s  << EOF \n %d,%d \n EOF'%(pdbfile, pdbname, rID, lID)
-        print(cmd)
-        os.system(cmd)
-        EF = os.path.exists('/tmp/%s/FINAL_RESULTS_MMPBSA.dat'%pdbname)
-        self.assertTrue(EF)
-        os.system('rm -rf /tmp/%s/'%pdbname)
-    
-    def pipline_traj(self, pdbfile, trajfile, rID, lID):
-        pdbname = os.path.split(pdbfile)[-1]
-        #cmd = 'hmtpbsa -i %s -o /tmp/%s > hmtpbsa.log 2>&1 << EOF \n %d,%d \n EOF'%(pdbfile, pdbname, rID, lID)
-        cmd = 'hmtpbsa -i %s -t %s -o /tmp/%s  << EOF \n %d,%d \n EOF'%(pdbfile, trajfile, pdbname, rID, lID)
-        print(cmd)
-        os.system(cmd)
-        EF = os.path.exists('/tmp/%s/FINAL_RESULTS_MMPBSA.dat'%pdbname)
-        self.assertTrue(EF)
-        os.system('rm -rf /tmp/%s/'%pdbname)
-
-    #def test_2fvy(self):
-    #    self.pipline_single('../example/2fvy.pdb', 1, 2)
-    
-    #def test_traj_ST(self):
-    #    self.pipline_traj('../example/ST/com.tpr', '../example/ST/com_traj.xtc', 1, 2)
-
 #################### version 0.0.2 ########################
 
     def pipeline_simulation(self, pdbfile, ligandfiles, configfile):
@@ -87,5 +59,6 @@ class TestPipline(unittest.TestCase):
         pdbfile = '../example/2fvy/protein.pdb'
         ligandfiles = ['../example/2fvy/BGC.mol2']
         self.pipeline_md(pdbfile, ligandfiles)
+
 if __name__ == "__main__":
     unittest.main()
