ad05162b13ddc99868dfc02696809fece0c139e3
Maohua Yang <maohuay@hotmail.com>
2022-04-17 01:25:26 +0800

    version 0.0.3 (#5)
    * tests: readd example file
    
    Former-commit-id: 8d1cbb5b7932715d4e9bdb15f1271dfd59563ab6 [formerly 6e874d1499de3dee7e23282790402b64e3397b75] [formerly 2b4d646122d87a759fb64a8ef06749f81d622b6e [formerly ef04d6c954bd5573be42a72d498eec0ef43d0c14]]
    Former-commit-id: 2b4d646122d87a759fb64a8ef06749f81d622b6e
    Former-commit-id: 93f8ca76594f4ca28033e07357a6b09616d95db9
    
    * style: add main.py
    
    * refactor: add some parameter
    
    * fix: var name error
    
    * fix: configure file name
    
    * feat: update gmx_MMPBSA to 1.5.2
    
    * doc: version 0.0.3
    
    * doc: update readme for v0.0.3

--- hmtpbsa/settings.py
+++ hmtpbsa/settings.py
@@ -1,9 +1,15 @@
 import os
+import re
+import sys
+import uuid
+import shutil
 import logging
 
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
-DEFAULT_CONFIGURE_FILE = os.path.dirname(os.path.abspath(__file__))+ '/data/detault.ini'
+DEFAULT_CONFIGURE_FILE = os.path.dirname(os.path.abspath(__file__))+ '/data/default.ini'
+
+PBSA_PARAMETER_FILE = os.path.dirname(os.path.abspath(__file__))+ '/data/mmpbsa.in'
 
 # base configure
 GMXEXE='gmx'
@@ -22,3 +28,21 @@ if 'OMP_NUM_THREADS' in os.environ:
     OMP_NUM_THREADS = os.environ['OMP_NUM_THREADS']
 else:
     OMP_NUM_THREADS = 4
+
+def obtain_MMPBSA_version():
+    versionFile = '/tmp/' + uuid.uuid1().hex
+    os.system('gmx_MMPBSA -v >%s 2>&1 '%versionFile)
+    with open(versionFile) as fr:
+        text = fr.read()
+        p = re.compile('v(\d\.\d)\.\d', re.S)
+        m = re.search(p, text)
+    os.system('rm %s'%versionFile)
+    if m:
+        version = float(m.groups()[0])
+    else:
+        logging.error('Can not found gmx_MMPBSA version.')
+        print(text)
+        sys.exit(1)
+    return version
+
+PBSA_VERSION = 1.5  #obtain_MMPBSA_version()
