4e9be2ff7c56a1d8ce9a81af98a7ec05824a5036
MaohuaYang <maohuay@hotmail.com>
2022-05-03 23:12:34 +0800

    doc: preversion of 0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
 ]
 setup(
     name = 'hmtpbsa',
-    version='0.0.4',
+    version='0.0.5_pre',
     author='dptech.net',
     author_email='hermite@dptech.net',
     description=('MMBPSA tools for calculate energy.'),
